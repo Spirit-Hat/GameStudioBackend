7401967ec688f697e808bec7006576e7bff86378
Spirit-Hat <vladonline34@gmail.com>
2022-07-18 10:18:55 +0300

    Initial commit

---
+++ src/test/java/com/example/gamestudio/GameStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamestudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
